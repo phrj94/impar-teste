88dc800fd550c18ac0573fdc7cfe9a337ce1107b
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-10 20:34:21 -0300

    using created components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "./components/header";
+import TopBar from "./components/topbar";
 import Theme from "./style/theme";
 
 const ContainerApp = styled.div`
   width: 100vw;
   height: 100vh;
   background: ${props => props.theme.colors.background};
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+  margin-top: 64px;
+  display: flex;
+  flex-direction: column;
 `;
 
 function App() {
   return (
     <Theme>
-      <ContainerApp></ContainerApp>
+      <ContainerApp>
+        <TopBar/>
+        <Main>
+          <Header></Header>
+        </Main>
+      </ContainerApp>
     </Theme>
   );
 }
