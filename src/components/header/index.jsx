914272d5bfe784115b23cd89de63f63759e51d98
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-10 20:31:08 -0300

    creating header of the app

---
+++ src/components/header/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderBackground from '../../assets/fundo-busca.png'
+import Search from "../search";
+
+const HeaderContainer = styled.header`
+    background-image: url(${HeaderBackground});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 261px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+`;
+
+const Header = () => {
+    return (
+        <HeaderContainer >
+           <Search/>
+
+        </HeaderContainer>
+    )
+}
+
+export default Header;
