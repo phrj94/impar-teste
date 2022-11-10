70f9dc56c6daa1866d8a42aa66199bd27d7f6ea7
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-10 20:30:17 -0300

    creating search bar

---
+++ src/components/search/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { ReactComponent as Icon } from '../../assets/lupa.svg'
+
+const SearchContainer = styled.div`
+    width: 93.9%;
+    max-width: 1046px;
+    height: 75px;
+    margin-bottom: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+`;
+
+
+const SearchIcon = styled(Icon)`
+   position: absolute;
+   padding-right: 25px;
+   :hover {
+        cursor: pointer;
+    }
+`;
+
+
+const SearchBar = styled.input`
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    border: ${props => props.theme.borderDefault};
+    
+    padding-left: 28px;
+    :focus-visible {
+        outline: none;
+    }
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const Search = () => {
+    const [search, setSearch] = useState('')
+    return (
+        <SearchContainer>
+            <SearchBar type="search" placeholder="Digite aqui sua busca..." value={search} onChange={(e) => setSearch(e.target.value)} />
+            <SearchIcon />
+        </SearchContainer>
+    )
+}
+
+export default Search;
