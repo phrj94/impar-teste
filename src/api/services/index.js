6f41cdb0292d13300e72a7986a8afbefa41475d7
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-13 09:21:52 -0300

    implemented api calls

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,1 +1,29 @@
 import api from '../index';
+
+export const getPokemons = async (limit, offset) => {
+    try {
+       const result = await (await api.get(`pokemon/?limit=${limit}&offset=${offset}`)).data;
+       return result;
+    } catch (error) {
+        console.error("Error to get pokemons. See the error: ", error)
+    }
+}
+
+export const getPokemonInfo = async (name) => {
+    try {
+        const result = await (await api.get(`pokemon/${name}/`)).data;
+        return result;
+    } catch (error) {
+        console.error("Error to get pokemon infos. See the error: ", error)
+    }
+}
+
+export const getAbility = async (name) => {
+    try {
+        const result = await (await api.get(`ability/${name}/`)).data;
+        return result;
+    } catch (error) {
+        console.error("Error to get ability. See the error: ", error)
+    }
+}
+
