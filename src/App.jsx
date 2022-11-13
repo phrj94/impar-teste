62fc435b89a97b188e9a635d4d5b6e4f784dc48a
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-13 09:32:27 -0300

    using content component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import Content from "./components/content";
 import Header from "./components/header";
 import TopBar from "./components/topbar";
 import Theme from "./style/theme";
 
 const ContainerApp = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   background: ${props => props.theme.colors.background};
   display: flex;
   flex-direction: column;
 `;
 
 const Main = styled.main`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   margin-top: 64px;
   display: flex;
   flex-direction: column;
@@ -27,6 +28,7 @@ function App() {
         <TopBar/>
         <Main>
           <Header></Header>
+          <Content/>
         </Main>
       </ContainerApp>
     </Theme>
