170abc3462e3efe080c7ee27ac2c0f00dc5a5f3e
Paulo Henrique <ph.rj.94@hotmail.com>
2022-11-13 16:21:05 -0300

    Implement search

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Icon } from '../../assets/lupa.svg'
+import Context from "../../context";
 
 const SearchContainer = styled.div`
     width: 93.9%;
@@ -17,9 +18,7 @@ const SearchContainer = styled.div`
 const SearchIcon = styled(Icon)`
    position: absolute;
    padding-right: 25px;
-   :hover {
-        cursor: pointer;
-    }
+   
 `;
 
 
@@ -30,6 +29,7 @@ const SearchBar = styled.input`
     border: ${props => props.theme.borderDefault};
     
     padding-left: 28px;
+    padding-right: 110px;
     :focus-visible {
         outline: none;
        
@@ -40,7 +40,20 @@ const SearchBar = styled.input`
 `;
 
 const Search = () => {
-    const [search, setSearch] = useState('')
+    const { search, setSearch, pokemonCards, setSearchedPokemonCards } = useContext(Context);
+
+    const getPokemonsByName = useCallback(() => {
+
+        const pokemonsByName = pokemonCards.filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+
+        return pokemonsByName;
+    }, [pokemonCards, search])
+
+    useEffect(() => {
+        if (!search) setSearchedPokemonCards([]);
+        else setSearchedPokemonCards(getPokemonsByName());
+    }, [search])
+
     return (
         <SearchContainer>
             <SearchBar type="search" placeholder="Digite aqui sua busca..." value={search} onChange={(e) => setSearch(e.target.value)} />
